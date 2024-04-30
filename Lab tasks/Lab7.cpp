0d688982130b6c3ad7007fcc0588238e79e7961d
GWASILI <bed-com-58-22@unima.ac.mw>
2024-04-30 20:25:11 +0200

    lab 5 and 6 answers

---
+++ Lab tasks/Lab7.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+// Function declaration
+void SwapNumbers(int& a, int& b);
+
+int main(){ 
+int varA = 25; 
+int varB = 100; 
+
+cout << "varA before swap: " << varA << endl; //varA is 25 
+cout << "VarB before swap: " << varB << endl; //varB is 100 
+SwapNumbers(varA, varB);
+
+cout << "varA after swap: " << varA << endl; //varA is 100 
+cout << "VarB after swap: " << varB << endl; //varB is 25 
+return 0; 
+}
+
+// Function definition
+void SwapNumbers(int& a, int& b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
